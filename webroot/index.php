0395505e4bb9856ca222b1f0290657dde4333ed3
Michael Zender <michael@crazymonkeys.de>
2011-08-09 22:52:48 +0200

    changed the way how the cake directory is located

--- webroot/index.php
+++ webroot/index.php
@@ -50,7 +50,7 @@
  *
  */
 	if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-		define('CAKE_CORE_INCLUDE_PATH', 'D:' . DS . 'Libraries' . DS . 'cakephp-1.3.11');
+		define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'tourdb_cakephp');
 	}
 
 /**
