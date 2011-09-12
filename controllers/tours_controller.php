e5623d7a7b969b03f646803445306a6778c89cdb
Michael Zender <michael@crazymonkeys.de>
2011-09-12 16:10:16 +0200

    refs #6 added new details to the tour tooltip

--- controllers/tours_controller.php
+++ controllers/tours_controller.php
@@ -74,7 +74,7 @@ class ToursController extends AppController
 	{
 		$this->set(array(
 			'tours' => $this->Tour->getCalendarData($year, $month, array(
-				'contain' => array('TourGuide', 'TourGuide.Profile', 'TourType', 'ConditionalRequisite', 'Difficulty')
+				'contain' => array('TourGuide', 'TourGuide.Profile', 'TourType', 'ConditionalRequisite', 'Difficulty', 'TourStatus')
 			)),
 			'month' => $month,
 			'year' => $year
